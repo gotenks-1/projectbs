d6ddb26571fd601e7780dfb1c78649be67937971
Ankit2997 <ankit29996@gmail.com>
2017-06-19 02:29:00 +0530

    header side nav

--- bin/header.php
+++ bin/header.php
@@ -21,8 +21,7 @@ if(!validate()){
 <script type="text/javascript" src="../js/cookiejs.js"></script>
 <script type="text/javascript" src="../js/materialize.js"></script>
 
-<body>
-
+<body >
 
 
 <!-- Dropdown logout -->
@@ -44,42 +43,58 @@ if(!validate()){
   <li><a href="#!">one</a></li>
   <li class="divider"></li>
   <li><a href="#!">two</a></li>
-
 </ul>
-<nav class="cyan darken-3" >
-  <div class="row">
-   <div class="">
- 		  <a href="#!" class="brand-logo left hide-on-small-only"><img  src="../sources/images/logos/logoiimt.png" alt="iimtlogo"  class="responsive-img" style="max-height:60px !important"  ></a>
-   		  <a href="#!" class="brand-logo right hide-on-med-and-up"><img  src="../sources/images/logos/logoiimt.png" alt="iimtlogo"  class="responsive-img" style="max-height:60px !important"  ></a>
-       <ul class="right ">
-          <li class=""><a class="dropdown-button " href="#!" data-activates="Notificationslarge"><span class="hide-on-small-only">Notifications</span><i class="material-icons right hide-on-small-only">add_alert</i></a></li>
-          <li><a class="dropdown-button" href="#!" data-activates="UserName"><span class="hide-on-small-only">UserName</span><i class="material-icons right hide-on-small-only">perm_identity</i></a></li>
-       </ul>
-    </div>
-    <!-- Side Navigation for small screen -->
-		  <ul id="slide-out" class="side-nav hide-on-med-and-up">
-		    <li><div class="userView">
-		      <div class="background ">
-		        <img src="../sources/images/logos/profile_bg.png">
-		      </div>
-		      <a href="#!user"><img class="circle" src="../sources/images/logos/logoiimt.png" alt="Profile"></a>
-		      <a href="#!name"><span class="white-text name">User Name</span></a>
-		      <a href="#!email"><span class="white-text email">abc@gmail.com</span></a>
-		    </div></li>
-		  	  <li class=""><a class="dropdown-button " href="#!" data-activates="NotificationsSmall
-		  	  "><i class="material-icons right">add_alert</i>Notifications</a></li>
-		  	  <li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
-		   	  <li><a href="#!">Second Link</a></li>
-		      <li><div class="divider"></div></li>
 
-		      <li><a id="mobile-logout" class="waves-effect" href="#!">Logout</a></li>
-		  </ul>
-		  <div class="">
-		  <a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>
-			</div>
-	  </div>
+<div class="row l1">
+	<nav class="cyan darken-3" >
+		  <div class="row" style="margin-bottom:0px;">
+		   <div>
+		 		  <a href="#!" class="brand-logo left hide-on-small-only"><img  src="../sources/images/logos/logoiimt.png" alt="iimtlogo"  class="responsive-img" style="max-height:60px !important"  ></a>
+		   		  <a href="#!" class="brand-logo right hide-on-med-and-up"><img  src="../sources/images/logos/logoiimt.png" alt="iimtlogo"  class="responsive-img" style="max-height:60px !important"  ></a>
+		       <ul class="right">
+		          <li class=""><a class="dropdown-button " href="#!" data-activates="Notificationslarge"><span class="hide-on-small-only">Notifications</span><i class="material-icons right hide-on-small-only">add_alert</i></a></li>
+		          <li><a class="dropdown-button" href="#!" data-activates="UserName"><span class="hide-on-small-only">UserName</span><i class="material-icons right hide-on-small-only">perm_identity</i></a></li>
+		       </ul>
+		    </div>
+		    <!-- Side Navigation for small screen -->
+				  <ul id="slide-out" class="side-nav hide-on-med-and-up">
+				    <li><div class="userView">
+				     	 <div class="background ">
+				        	<img src="../sources/images/logos/profile_bg.png">
+				      	 </div>
+				      		<a href="#!user"><img class="circle" src="../sources/images/logos/logoiimt.png" alt="Profile"></a>
+				      		<a href="#!name"><span class="white-text name">User Name</span></a>
+				      		<a href="#!email"><span class="white-text email">abc@gmail.com</span></a>
+				        </div>
+				    </li>
+				  	  
+				  	  <li class="">
+				  	  <a class="dropdown-button " href="#!" data-activates="NotificationsSmall"><i class="material-icons">add_alert</i>Notifications</a></li>
+				  	  <li><a href="#!">INVITES</a></li>
+				   	  <li><a href="#!">SOMETHING</a></li>
+				      <li><div class="divider"></div></li>
 
-</nav>
+				      <li><a id="mobile-logout" class="waves-effect" href="#!">Logout</a></li>
+				  </ul>
+				  <div class="">
+				  		<a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>
+				  </div>
+			  </div>
+		</nav>
+	 <div class="row hide-on-small-only">
+       <div class="col l2 w3-container" style="background:#00838f; height: 100%;">
+       	   <div class="card cyan darken-2" style="height: 100%;">
+            <div class="card-content white-text">
+           		<div class="card-action">
+		           	<a href="#" class="waves-effect btn-flat white-text">Invites</a>
+		        </div>
+      			<div class="card-action">
+		     		<a href="#" class="waves-effect btn-flat white-text">Invites</a>
+        		</div>
+            </div>
+          </div>
+        </div>
+ 	</div>
 
 
 
