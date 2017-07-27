04707b44315fe05601afae834032cac2db64fb15
Lawliet-01 <ankit29996@gmail.com>
2017-07-27 12:44:52 +0530

    msg

--- bin/querypage.php
+++ bin/querypage.php
@@ -8,32 +8,29 @@ include 'dbconn.php';
 
 	if(isset($_COOKIE['userid']))
 	{
-		   $query= "SELECT type From LoginDetail Where userid='".$_COOKIE['userid']."'";
+			$userid=$_COOKIE['userid'];
+			 $query= "SELECT type From LoginDetail Where userid='".$userid."'";
 
 		  $result=mysqli_query($conn,$query);
 
 		 $dbarray = mysqli_fetch_object($result);
 
 		 $val=$dbarray->type;
-
+    var_dump($val);
 		 Mysqli_free_result($result);
 
 		 if(isset($_POST['submit_btn']) && $val=="normal")
 		 {
+
 			 $qu_txt= mysqli_real_escape_string($conn,$_POST['qu_txt']);
 			 $subject= mysqli_real_escape_string($conn,$_POST['subject']);
 			 $userid =$_COOKIE['userid'];
-
-			 $query= "INSERT INTO Queries(userid,subject,query) Values('$userid','$subject','$qu_txt')";
+			 $query= "INSERT INTO queries(userid,subject,query) Values('$userid','$subject','$qu_txt')";
 
 			 if(mysqli_query($conn,$query))
 			 {
 
-			 	echo	'<script type="text/javascript">';
-			 	echo    'alert("ankit)';
-				echo    '</script>';
-
-	 			header("Location: ../index.php");
+				 header("Location: ../index.php");
 
 			 }
 			 else
@@ -45,26 +42,6 @@ include 'dbconn.php';
 	}
 ?>
 
-<!-- <DOCTYPE html>
-
-<html lang=en>
-
-<head> -->
-  <!-- <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"> -->
-   <!-- <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
-</head>
-
-<link rel="stylesheet" type="text/css" href ="../css/materialize.css"/>
-<link rel="stylesheet" type="text/css" href ="../css/styl.css"/>
-<script type="text/javascript" src="../js/jquery.js"></script>
-<script type="text/javascript" src="../js/cookiejs.js"></script>
-<script type="text/javascript" src="../js/materialize.js"> </script>
-
-
- <body>
-  <div class="container"> -->
-  		<!-- <div class="card-panel teal lighten-2"> -->
 
   		<h5 class="card-panel teal lighten-2" style="color:white">QUERY</h5>
 	  <div class="">
